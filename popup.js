541ab99d4d3c579b123d5ed829574e444f1cc9e9
asb1512 <andrew@olympicmedia.com>
2022-03-14 23:37:31 -0400

    building out logic for toggle buttons; persisting state with storage API

--- popup.js
+++ popup.js
@@ -1,2 +1,22 @@
+const state = {};
+
 const onButton = document.getElementById('on');
 const offButton = document.getElementById('off');
+
+function styleElementOnClick(element) {
+  element.style.color = '#'
+  element.style.backgroundColor = '#FBFFFE';
+  element.style.color = '#0A0A0B';
+}
+
+function toggleExtension(newState) {
+  switch (newState) {
+    case "on":
+      break;
+    case "off":
+      break;
+  }
+}
+
+onButton.addEventListener("click", toggleExtension("on"));
+offButton.addEventListener("click", toggleExtension("off"));
